dc4a9ff9b3118b8865f3bb4cab553df4a9263f82
ynonp <ynonperek@gmail.com>
2023-01-28 17:01:02 +0200

    update list in requesting page

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,24 @@
 class MessagesController < ApplicationController
   def index
     @messages = Message.all
-    @message = Message.new
+    @new_message = Message.new
   end
 
   def create
     @message = Message.new(message_params)
-    @message.save!
-
-    redirect_to messages_path
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to messages_path }
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.append('messages', partial: 'message', locals: { message: @message }),
+            turbo_stream.replace('new_message', partial: 'new_message_form')
+          ]
+        end
+      else
+        format.html { render :index, status: :unprocessable_entity }
+      end
+    end
   end
 
   def message_params
